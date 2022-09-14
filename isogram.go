2aa64442a7fdc0890ee4b8da14719cef955670c9
Nasir Lawal <nasirlawal001@gmail.com>
2022-09-14 21:51:29 +0100

    solved scrabble score

---
+++ isogram.go
@@ -0,0 +1,21 @@
+func IsIsogram(word string) bool {
+	seen := make([]bool, 1<<8)
+
+	for _, char := range word {
+		// Deal only with uppercase letters.
+		if 97 <= char && char <= 122 {
+			char -= 32
+		}
+
+		// Make sure we only add uppercase letters.
+		if 65 <= char && char <= 90 {
+			if seen[char] {
+				return false
+			}
+
+			seen[char] = true
+		}
+	}
+
+	return true
+}
